7550911a07e1b57288e271ea346606affeb8d4e5
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-09-28 17:46:23 -0500

    added description

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -8,9 +8,11 @@ import java.util.Objects;
 public class Event {
 
     private String name;
+    private String description;
 
-    public Event(String name) {
+    public Event(String name, String description) {
         this.name = name;
+        this.description = description;
     }
 
     public String getName() {
@@ -21,9 +23,16 @@ public class Event {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         return name;
     }
-
 }
